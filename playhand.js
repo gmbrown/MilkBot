a562545869d50eff01038bc7b681310c4f56cb35
Gregory Brown <gbrown@Gregorys-MacBook-Pro.local>
2022-01-27 18:38:32 -0800

    add some more betting logic

--- playhand.js
+++ playhand.js
@@ -32,14 +32,37 @@ function goAllIn() {
     game.action_widget.execute_bet_raise()
 }
 
-playhand = function(handString) {
+playhand = function(handString, boardString) {
     if (game.ruleset_name !== 'NL Texas Holdem') {
         console.log(`Folding/checking because we aren't playing 'NL Texas Holdem'. The game is ${game.ruleset_name}.`)
         fold()
         return;
     }
+
+    if (boardString === "") {
+        preflop(handString)
+    }
+    else {
+        postflop(handString, boardString)
+    }
+
+    handRanksString = cards[0].rank + cards[1].rank
+
+    // On these hands go all in
+    allInRanks = ["72", "AA", "KK", "QQ", "JJ", "1010", "99", "88", "77", "66", "55", "44", "33", "22", "AK", "AQ", "AJ"]
+    if (allInRanks.includes(handRanksString)) {
+        goAllIn()
+    } else {
+        fold()
+    }
+
+    preflopCallRanks = ["77", "66", "55", "44", "33", "22"]
+    if (preflopCallRanks)
+}
+
+function cardStringToObj(cardsString) {
     cards = [];
-    handString.split("?").forEach(cardString => {
+    cardsString.split("?").forEach(cardString => {
         if (cardString === "") {
             return;
         }
@@ -50,10 +73,10 @@ playhand = function(handString) {
         else {
             rank = cardString[0]
         }
-        console.log("rank: " + rank)
-        console.log("suit: " + suit)
+        console.log("rank: " + rank);
+        console.log("suit: " + suit);
         
-        ranknum = 0
+        ranknum = 0;
         switch(rank) {
             case 'J':
                 ranknum = 11;
@@ -71,25 +94,84 @@ playhand = function(handString) {
                 ranknum = parseInt(rank)
         }
         // Rank number should go 0 - 12
-        ranknum -= 2
+        ranknum -= 2;
 
         card = {
             'suit': suit,
             'rank': rank,
             'ranknum': ranknum
-        }
-        cards.push(card)
+        };
+        cards.push(card);
     })
-    cards.sort((a,b) => (b.ranknum - a.ranknum))
+    cards.sort((a,b) => (b.ranknum - a.ranknum));
+    return cards
+}
 
+function preflop(cardsString) {
+    cards = cardStringToObj(cardsString)
     handRanksString = cards[0].rank + cards[1].rank
 
     // On these hands go all in
-    allInRanks = ["72", "AA", "KK", "QQ", "JJ", "1010", "99", "88", "77", "66", "55", "44", "33", "22", "AK", "AQ", "AJ"]
+    allInRanks = ["72", "AA", "KK", "QQ", "JJ", "1010", "99", "88", "AK", "AQ", "AJ"];
     if (allInRanks.includes(handRanksString)) {
         goAllIn()
-    } else {
-        fold()
+        return
     }
 
+    preflopBetRanks = ["KQ", "KJ", "A10"];
+    if (preflopBetRanks.includes(handRanksString)) {
+        makePotSizeBet()
+        return
+    }
+
+    preflopCallRanks = ["77", "66", "55", "44", "33", "22", "A9", "A8"];
+    if (preflopCallRanks.includes(handRanksString)) {
+        callOrCheck()
+        return
+    }
+
+    fold()
+}
+
+function postflop(cardsString, boardCardsString) {
+    cards = cardStringToObj(cardsString)
+    boardCards = cardStringToObj(boardCardsString)
+
+    if (cards[0].rank === cards[1].rank) {
+        // Pocket pairs
+        // Will go all in with trips or fold
+        boardCards.forEach((boardCard) => {
+            if (boardCard.rank === cards[0].rank) {
+                // hit trips
+                console.log("we hit trips")
+                goAllIn()
+                return
+            }
+        });
+    }
+
+    boardCards.forEach((boardCard) => {
+        if (boardCard.rank === cards[0].rank || boardCard.rank === cards[1].rank) {
+            // paired up
+            console.log("we hit a pair")
+            makePotSizeBet()
+            return
+        } 
+    })
+
+    // See if we have a flush
+    suitFreqWithBoard = {"d": 0, "h": 0, "c": 0, "s": 0}
+    cards.forEach( (card) => {
+        suitFreqWithBoard[card.suit] += 1
+    })
+    boardCards.forEach( (card) => {
+        suitFreqWithBoard[card.suit] += 1
+    })
+    if (Math.max(...Object.values(suitFreqWithBoard) >= 5)) {
+        console.log("we have a flush")
+        goAllIn()
+        return
+    }
+    
+    fold()
 }
