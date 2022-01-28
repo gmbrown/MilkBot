4ba92323eaf3156d35da39105a11259dc088c102
Jessica Bethune <3413059+jbthn@users.noreply.github.com>
2022-01-28 11:24:32 -0800

    various fixes from week 1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Poker Bot For Donkhouse
 ## Using MilkBot
 When you're on the donkhouse.com website, paste this code snippet into your console to load MilkBot and let it play for you:
 ```
-fetch("https://raw.githubusercontent.com/gmbrown/MilkBot/main/playhand.js")
+fetch("https://raw.githubusercontent.com/gmbrown/MilkBot/main/playhand.js", {cache: "no-store"})
     .then(r => r.text())
     .then(t => eval(t))
 ```
--- playhand.js
+++ playhand.js
@@ -1,10 +1,18 @@
-setInterval(checkIfTurnAndPlay, 1000)
+setInterval(checkIfTurnAndPlay, 2500)
+socket.on('distributing pot', checkWhoWonAndMaybeTaunt)
 
 function checkIfTurnAndPlay () {
-    if (!game.action_widget) {
+    if (!game.action_widget || !game.players[game.client_perspective].cards.card_str) {
+        // seems like sometimes action_widget will be truthy but there are no cards... skip
         return
     }
-    playhand(game.players[game.action_widget.seat].cards.card_str, game.board.card_str)
+    playhand(game.players[game.client_perspective].cards.card_str, game.board.card_str)
+}
+
+function showCardsAtEndOfHand() {
+    if (game.game_options_widget.allow_easy_reveal && !game.game_options_widget.easy_reveal.is_checked()) {
+        game.game_options_widget.easy_reveal.make_selected()
+    }
 }
 
 function fold() {
@@ -19,10 +27,29 @@ function callOrCheck() {
     game.action_widget.execute_check_call()
 }
 
-function makePotSizeBet() {
-    const potSizeIfCall = game.action_widget.pot_size - game.action_widget.bet_in_front + game.action_widget.last_bet;
-    game.action_widget.update_slider_by_value(potSizeIfCall + game.action_widget.last_bet);
-    game.action_widget.execute_bet_raise();
+function makePotSizedBet() {
+    const minIndex = game.action_widget.threshold_tags.indexOf('min');
+    const potThresholdIndex = game.action_widget.threshold_tags.indexOf('pot') - minIndex;
+    const numXValues = game.action_widget.x_values.length;
+    const shouldUseAllInXValue = potThresholdIndex === -1 || potThresholdIndex + 1 > numXValues
+    const xValueIndex = shouldUseAllInXValue ? numXValues - 1 : potThresholdIndex
+    try {
+        game.action_widget.update_sizing_input_by_position(game.action_widget.x_values[xValueIndex])
+        game.action_widget.execute_bet_raise();
+    } catch (e) {
+        console.error(e); // probably this will only throw if there is no option to raise
+        console.log(`Expecting bet_button to be falsy and it is: ${game.action_widget.bet_button}`)
+        console.log(`Expecting raise_button to be falsy and it is: ${game.action_widget.raise_button}`)
+        if (game.action_widget.all_in) {
+            console.log('Seems like the all-in button is visible. Going all in.')
+            game.action_widget.all_in.execute()
+        } else if (game.action_widget.call_button) {
+            console.log('Seems like the call button is visible, but not all-in. Calling.')
+            game.action_widget.call_button.execute()
+        } else {
+            console.error('Not sure what to do. HELP.')
+        }
+    }
 }
 
 function goAllIn() {
@@ -38,6 +65,7 @@ playhand = function(handString, boardString) {
         fold()
         return;
     }
+    showCardsAtEndOfHand()
 
     if (boardString === "") {
         preflop(handString)
@@ -53,16 +81,14 @@ function cardStringToObj(cardsString) {
         if (cardString === "") {
             return;
         }
-        var suit = cardString[cardString.length - 1];
-        if (cardString.length > 2) {
-            var rank = "10"
-        }
-        else {
-            var rank = cardString[0]
-        }
+        var rank = cardString[0];
+        var suit = cardString[1];
         
         var ranknum = 0;
         switch(rank) {
+            case 'T':
+                ranknum = 10;
+                break;
             case 'J':
                 ranknum = 11;
                 break;
@@ -97,17 +123,17 @@ function preflop(cardsString) {
     var handRanksString = cards[0].rank + cards[1].rank
 
     // On these hands go all in
-    var allInRanks = ["72", "AA", "KK", "QQ", "JJ", "1010", "99", "88", "AK", "AQ", "AJ"];
+    var allInRanks = ["72", "AA", "KK", "QQ", "JJ", "TT", "99", "88", "AK", "AQ", "AJ"];
     if (allInRanks.includes(handRanksString)) {
         console.log(`My hand is ${cardsString} pre-flop. Going all in.`)
         goAllIn()
         return
     }
 
-    var preflopBetRanks = ["KQ", "KJ", "A10"];
+    var preflopBetRanks = ["KQ", "KJ", "AT"];
     if (preflopBetRanks.includes(handRanksString)) {
         console.log(`My hand is ${cardsString} pre-flop. Making pot-sized bet.`)
-        makePotSizeBet()
+        makePotSizedBet()
         return
     }
 
@@ -118,40 +144,46 @@ function preflop(cardsString) {
         return
     }
 
-    console.log(`My hand is ${cardsString} pre-flop. Folding.`)
+    console.log(`My hand is ${cardsString} pre-flop. Folding or checking.`)
     fold()
 }
 
-function postflop(cardsString, boardCardsString) {
+function postflop(cardsString, boardCardsString) {    
     var cards = cardStringToObj(cardsString)
     var boardCards = cardStringToObj(boardCardsString)
 
     if (cards[0].rank === cards[1].rank) {
         // Pocket pairs
         // Will go all in with trips or fold
+        var hitTrips = false;
         boardCards.forEach((boardCard) => {
             if (boardCard.rank === cards[0].rank) {
-                // hit trips
+                hitTrips = true
                 console.log("we hit trips")
-                console.log(`My hand is ${cardsString} and the board shows ${boardCardsString}. Going all in.`)
-                goAllIn()
-                return
             }
         });
+        if (hitTrips) {
+            console.log(`My hand is ${cardsString} and the board shows ${boardCardsString}. Going all in.`)
+            goAllIn()
+            return
+        }
     }
 
+    var hitPair = false
     boardCards.forEach((boardCard) => {
         if (boardCard.rank === cards[0].rank || boardCard.rank === cards[1].rank) {
-            // paired up
+            hitPair = true;
             console.log("we hit a pair")
-            console.log(`My hand is ${cardsString} and the board shows ${boardCardsString}. Making pot-sized bet.`)
-            makePotSizeBet()
-            return
         } 
     })
+    if (hitPair) {
+        console.log(`My hand is ${cardsString} and the board shows ${boardCardsString}. Making pot-sized bet.`)
+        makePotSizedBet()
+        return
+    }
 
     // See if we have a flush
-    var suitFreqWithBoard = {"d": 0, "h": 0, "c": 0, "s": 0}
+    var suitFreqWithBoard = {"D": 0, "H": 0, "C": 0, "S": 0}
     cards.forEach( (card) => {
         suitFreqWithBoard[card.suit] += 1
     })
@@ -165,5 +197,24 @@ function postflop(cardsString, boardCardsString) {
         return
     }
     
+    console.log(`My hand is ${cardsString} and the board shows ${boardCardsString}. Folding or checking.`)
     fold()
+}
+
+function checkWhoWonAndMaybeTaunt(potData) {
+    const seat = game.client_perspective;
+    if (potData.winners.length > 1 || !potData.winners[seat]) {
+        // either there is more than one winner or someone other than the bot won. do nothing
+        return;
+    }
+    const knockedSomeoneOut = Object.keys(potData.stack_consistency_check)
+        .some(key => key !== seat && potData.stack_consistency_check[key] === 0)
+    // TODO if someone was knocked out a while ago would they show up here?
+    // or does it only include people from that hand?
+
+    if (knockedSomeoneOut) {
+        console.log(`I think I knocked someone out. Btw my seat number is ${seat}. Pot data:`)
+        console.dir(potData)
+        socket.emit('taunt', {taunt: 16, id: game.table_id, group_id: game.group_id})
+    }
 }
