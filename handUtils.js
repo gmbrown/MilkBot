8b21bc3cadf0c8fe0cabb570928ca71010b5e738
Jessica Bethune <3413059+jbthn@users.noreply.github.com>
2022-06-27 21:54:32 -0700

    Optimize winAgainstPercent calculations

--- handUtils.js
+++ handUtils.js
@@ -11,65 +11,56 @@ export function checkStraightOrDrawOfLength(allCards, lengthOfDraw) {
 
   for (let i = 0; i <= uniqueRanksInOrder.length - lengthOfDraw; i++) {
     const subStringToCheck = uniqueRanksInOrder.slice(i, i + lengthOfDraw);
-    if ('AKQJT98765432A'.indexOf(subStringToCheck) !== -1) {
+    if ('AKQJT98765432A'.includes(subStringToCheck)) {
       return subStringToCheck[0];
     }
   }
 }
 
 export function myPokerHand(handCards, boardCards) {
-  const rankToCount = {};
-  const suitToCount = {};
+  const rankToCount = new Map();
+  const suitToCount = new Map();
   const allCards = handCards.concat(boardCards);
+  allCards.sort((a, b) => b.ranknum - a.ranknum);
   allCards.forEach((card) => {
-    const currentRankCount = rankToCount[card.rank] || 0;
-    rankToCount[card.rank] = currentRankCount + 1;
-
-    const currentSuitCount = suitToCount[card.suit] || 0;
-    suitToCount[card.suit] = currentSuitCount + 1;
+    rankToCount.set(card.rank, (rankToCount.get(card.rank) || 0) + 1);
+    suitToCount.set(card.suit, (suitToCount.get(card.suit) || 0) + 1);
   });
 
-  let highestCount = 0;
-  let secondHighestCount = 0;
-  Object.entries(rankToCount).forEach(([_, count]) => {
-    if (count > highestCount) {
-      secondHighestCount = highestCount;
-      highestCount = count;
-    } else if (count > secondHighestCount) {
-      secondHighestCount = count;
+  let highestRankAndCount = { count: 0 };
+  let secondHighestRankAndCount = { count: 0 };
+  rankToCount.forEach((count, rank) => {
+    // maps preserve insertion order, so we know higher ranks always come first
+    if (count > highestRankAndCount.count) {
+      secondHighestRankAndCount = highestRankAndCount;
+      highestRankAndCount = { rank, count };
+    } else if (count > secondHighestRankAndCount.count) {
+      secondHighestRankAndCount = { rank, count };
     }
   });
 
   // Check straight flush
-  var hasFlush = false;
-  var flushRanks = [];
-  var staightFlushTopRank;
-  Object.entries(suitToCount).forEach(([suit, count]) => {
+  let flushRanks;
+  let straightFlushTopRank;
+  suitToCount.forEach((count, suit) => {
     if (count >= 5) {
-      hasFlush = true;
       const cardsOfSuit = allCards.filter((card) => card.suit === suit);
-      flushRanks = cardsOfSuit
-        .sort((a, b) => b.ranknum - a.ranknum)
-        .map((card) => card.rank)
-        .slice(0, 5);
-      staightFlushTopRank = checkStraightOrDrawOfLength(cardsOfSuit, 5);
+      flushRanks = cardsOfSuit.map((card) => card.rank).slice(0, 5);
+      straightFlushTopRank = checkStraightOrDrawOfLength(cardsOfSuit, 5);
     }
   });
 
-  if (staightFlushTopRank) {
+  if (straightFlushTopRank) {
     return {
       hand: HANDS.STRAIGHT_FLUSH,
-      handRanks: [staightFlushTopRank],
+      handRanks: [straightFlushTopRank],
       kickers: [],
     };
   }
 
   // Check 4 of a kind
-  if (highestCount === 4) {
-    const fourOfAKindRank = Object.entries(rankToCount).filter(
-      ([, count]) => count === 4
-    )[0][0];
-
+  if (highestRankAndCount.count === 4) {
+    const fourOfAKindRank = highestRankAndCount.rank;
     const cardsMinusFourOfAKind = allCards.filter(
       (card) => card.rank != fourOfAKindRank
     );
@@ -82,34 +73,19 @@ export function myPokerHand(handCards, boardCards) {
   }
 
   // Check full house
-  if (highestCount === 3 && secondHighestCount >= 2) {
-    var tripRanks = [];
-    var pairRanks = [];
-    for (let rank in rankToCount) {
-      if (rankToCount[rank] == 3) {
-        tripRanks.push(rank);
-      } else if (rankToCount[rank] == 2) {
-        pairRanks.push(rank);
-      }
-    }
-
-    tripRanks.sort((a, b) => RANK_TO_NUMRANK[b] - RANK_TO_NUMRANK[a]);
-
-    // If there are 2 separate 3 of a kinds on the board then we need to treat the lower one as a pair
-    if (tripRanks.length > 1) {
-      pairRanks.push(tripRanks[1]);
-    }
-    pairRanks.sort((a, b) => RANK_TO_NUMRANK[b] - RANK_TO_NUMRANK[a]);
+  if (highestRankAndCount.count === 3 && secondHighestRankAndCount.count >= 2) {
+    const tripRank = highestRankAndCount.rank;
+    const pairRank = secondHighestRankAndCount.rank;
 
     return {
       hand: HANDS.FULL_HOUSE,
-      handRanks: [tripRanks[0], pairRanks[0]],
+      handRanks: [tripRank, pairRank],
       kickers: [],
     };
   }
 
   // Check flush
-  if (hasFlush) {
+  if (flushRanks) {
     return {
       hand: HANDS.FLUSH,
       handRanks: flushRanks,
@@ -128,11 +104,8 @@ export function myPokerHand(handCards, boardCards) {
   }
 
   // Check 3 of a kind
-  if (highestCount === 3) {
-    const tripRank = Object.entries(rankToCount).filter(
-      ([, count]) => count === 3
-    )[0][0];
-
+  if (highestRankAndCount.count === 3) {
+    const tripRank = highestRankAndCount.rank;
     var cardsMinusTrips = allCards.filter((card) => card.rank != tripRank);
 
     return {
@@ -143,12 +116,14 @@ export function myPokerHand(handCards, boardCards) {
   }
 
   // Check 2 pair
-  if (highestCount === 2 && secondHighestCount === 2) {
-    const pairRanks = Object.entries(rankToCount)
-      .filter(([, count]) => count === 2)
-      .map(([rank]) => rank)
-      .sort((a, b) => RANK_TO_NUMRANK[b] - RANK_TO_NUMRANK[a])
-      .slice(0, 2);
+  if (
+    highestRankAndCount.count === 2 &&
+    secondHighestRankAndCount.count === 2
+  ) {
+    const pairRanks = [
+      highestRankAndCount.rank,
+      secondHighestRankAndCount.rank,
+    ];
 
     var cardsMinusPairs = allCards.filter(
       (card) => !pairRanks.includes(card.rank)
@@ -162,11 +137,8 @@ export function myPokerHand(handCards, boardCards) {
   }
 
   // Check pair
-  if (highestCount === 2) {
-    const pairRank = Object.entries(rankToCount).filter(
-      ([, count]) => count === 2
-    )[0][0];
-
+  if (highestRankAndCount.count === 2) {
+    const pairRank = highestRankAndCount.rank;
     var cardsMinusPair = allCards.filter((card) => card.rank != pairRank);
 
     return {
@@ -183,9 +155,7 @@ export function myPokerHand(handCards, boardCards) {
   };
 }
 
+// We're not sorting because we're assuming the cards are already sorted (beginning of myPokerHand)
 export function getTopNRanks(cards, n) {
-  return cards
-    .map((card) => card.rank)
-    .sort((a, b) => RANK_TO_NUMRANK[b] - RANK_TO_NUMRANK[a])
-    .slice(0, n);
+  return cards.map((card) => card.rank).slice(0, n);
 }
